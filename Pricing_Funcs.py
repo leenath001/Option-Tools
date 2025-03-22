e41de77d150945c21f2fa8211f1716a62b39f9b4
Nathaniel Lee <leenathaniel001@gmail.com>
2025-03-22 05:38:49 -0400

    Update Pricing_Funcs.py
    Added Black Scholes Pricing formula. Working to implement within IV solver

--- Pricing_Funcs.py
+++ Pricing_Funcs.py
@@ -1,7 +1,6 @@
+# IN PROGRESS
 def Binomial_Tree_Eur(S0,K,r,T,N,IV,type):
-## IN PROGRESS
 
-    import Param_Est
     import numpy as np
     import math as m
 
@@ -31,21 +30,36 @@ def Binomial_Tree_Eur(S0,K,r,T,N,IV,type):
     val = round(EVdisc,2)
 
     return val
-    
-def IV_solver(S0,K,r,T,N,type,MKT_price,IV_guess):
-    # instead of using a binomial tree model, solve the BS equation for IV
+
+# IN PROGRESS
+def Black_Scholes_Pricing(S0,K,r,xpiry,IV,type):
+
+    import datetime
+    import numpy as np
+    from scipy.stats import norm
+
+    # time to expiry as an int
+    today = datetime.date.today()
+    expiry = np.datetime64(xpiry)
+    ttxp = np.busday_count(today,expiry)
+
+    w = (np.log(S0/K) + (r + .5 * IV**2) * ttxp)/(np.sqrt(ttxp) * IV)
+
+    if type == 'call':
+        C = S0 * norm.cdf(w,loc = 0,scale = 1) - np.exp(-r*ttxp)*K*norm.cdf(w - IV*ttxp,loc = 0,scale = 1)
+        return C
+    elif type == 'put':
+        P = np.exp(-r*ttxp) * K * norm.cdf(-w + IV * ttxp,loc = 0,scale = 1) - S0 * norm.cdf(-w,loc = 0,scale = 1)
+        return P 
+
+# Working to integrate with BS
+def IV_solver(S0,K,r,xpiry,type,MKT_price):
     
     import scipy.optimize as opt
     
-    objective = lambda IV: Binomial_Tree_Eur(S0,K,r,T,N,IV,type) - MKT_price
-    sol = opt.brentq(objective,.001,6)
+    objective = lambda IV: Black_Scholes_Pricing(S0,K,r,xpiry,IV,type) - MKT_price
+    sol = opt.brentq(objective)
     
     return sol
-    
 
-
-
-
-    
-    
-    
+# my functions seem to be overshooting the value, why is this? Theoretical, or incorrect implementation?
