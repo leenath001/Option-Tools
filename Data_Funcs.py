f69f4bc5a091d36eb3db1d216d42d962260d8fbd
Nathaniel Lee <leenathaniel001@gmail.com>
2025-04-02 12:01:40 -0400

    Update Data_Funcs.py
    Added put/call ratio indicator

--- Data_Funcs.py
+++ Data_Funcs.py
@@ -90,4 +90,54 @@ def opt_data_IVchain(ticker):
     MASVF.index = range(len(MASVF))
 
     return MASVF
+
+def Put_Call_ratio(ticker):
+    import yfinance as yf
+    import Data_Funcs as df
+    import pandas as pd
+    import warnings
+    import numpy
+
+    warnings.filterwarnings("ignore")
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+
+    # getting most recent expiry date   
+    stoxx = yf.Ticker(ticker)
+    expirations = stoxx.options
+    expiry = expirations[0]
+
+    # getting chain
+    option_chain = stoxx.option_chain(expiry)
+    calls = option_chain.calls
+    x =  calls[['strike','volume']]
+    x1 = x[['volume']]
+       
+    puts = option_chain.puts
+    y = puts[['strike','volume',]]
+    y1 = y[['volume']]
+
+    # closest to money strike, OTM. 
+    S = df.equity_bidask(ticker) # tkr price
+    S = S[1]
+    xmod = calls[['strike']]-S
+    ymod = puts[['strike']]-S
+    C_first_positive = xmod.apply(lambda x: x[x > 0].index.min()) # first pos ind
+    P_first_positive = ymod.apply(lambda x: x[x > 0].index.min()) - 1 # last neg ind
+
+    # puts/calls, >1 -> p > c -> bearish, <1 -> p < c -> bullish
+
+    # implementing formula P_vol/C_vol
+    Cvol = x1.iloc[C_first_positive]
+    Pvol = y1.iloc[P_first_positive]
+    Cvol = Cvol.to_numpy()
+    Pvol = Pvol.to_numpy()
+
+    ratio = Pvol/Cvol
+    ratio = ratio[0]
+
+    return ratio[0]
+
+
+
         
