64b41dca8da6f96a7f010e77c0aac6e50297e6df
Nathaniel Lee <leenathaniel001@gmail.com>
2025-04-03 09:33:50 -0400

    Update Data_Funcs.py

--- Data_Funcs.py
+++ Data_Funcs.py
@@ -105,38 +105,43 @@ def Put_Call_ratio(ticker):
     # getting most recent expiry date   
     stoxx = yf.Ticker(ticker)
     expirations = stoxx.options
-    expiry = expirations[0]
 
-    # getting chain
-    option_chain = stoxx.option_chain(expiry)
-    calls = option_chain.calls
-    x =  calls[['strike','volume']]
-    x1 = x[['volume']]
+    for dates in expirations: 
+        expiry = dates
+
+        # getting chain
+        option_chain = stoxx.option_chain(expiry)
+        calls = option_chain.calls
+        x =  calls[['strike','volume']]
+        x1 = x[['volume']]
        
-    puts = option_chain.puts
-    y = puts[['strike','volume',]]
-    y1 = y[['volume']]
+        puts = option_chain.puts
+        y = puts[['strike','volume',]]
+        y1 = y[['volume']]
 
-    # closest to money strike, OTM. 
-    S = df.equity_bidask(ticker) # tkr price
-    S = S[1]
-    xmod = calls[['strike']]-S
-    ymod = puts[['strike']]-S
-    C_first_positive = xmod.apply(lambda x: x[x > 0].index.min()) # first pos ind
-    P_first_positive = ymod.apply(lambda x: x[x > 0].index.min()) - 1 # last neg ind
+        # closest to money strike, OTM. 
+        S = df.equity_bidask(ticker) # tkr price
+        S = S[1]
+        xmod = calls[['strike']]-S
+        ymod = puts[['strike']]-S
+        C_first_positive = (calls['strike'] - S).gt(0).idxmax()
+        P_first_positive = (puts['strike'] - S).gt(0).idxmax() - 1 
 
     # puts/calls, >1 -> p > c -> bearish, <1 -> p < c -> bullish
 
-    # implementing formula P_vol/C_vol
-    Cvol = x1.iloc[C_first_positive]
-    Pvol = y1.iloc[P_first_positive]
-    Cvol = Cvol.to_numpy()
-    Pvol = Pvol.to_numpy()
+        # implementing formula P_vol/C_vol
+        Cvol = x1.iloc[C_first_positive]
+        Pvol = y1.iloc[P_first_positive]
+        Cvol = Cvol.to_numpy()
+        Pvol = Pvol.to_numpy()
 
-    ratio = Pvol/Cvol
-    ratio = ratio[0]
+        ratio = Pvol/Cvol
+        ratio = ratio[0]
 
-    return ratio[0]
+        str = "{} Put to Call Ratio : {}".format(dates,ratio)
+        print(str)
+
+
 
 
 
